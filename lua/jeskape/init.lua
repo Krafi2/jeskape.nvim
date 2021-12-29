b208aca31d9e8396a44f9fb117d16b6bc3453ab2
Krafi <jakub.havlicek5@gmail.com>
2021-12-29 12:24:50 +0100

    Fix mappings not getting trigerred when preceded by some characters

--- lua/jeskape/init.lua
+++ lua/jeskape/init.lua
@@ -29,21 +29,13 @@ local function finish_mapping(output)
     return out
 end
 
--- This function runs on every keypress registered in a mapping.
--- This is where most of the plugin's logic is.
-local function _key_pressed(key)
-    mod.last = mod.last or vim.fn.reltime()
-    local now = vim.fn.reltime()
-    -- reltime is in seconds but we need miliseconds
-    local delta = vim.fn.reltimefloat(vim.fn.reltime(mod.last, now)) * 1000
-    local timeout = config.settings().timeout
-
-    -- Start a new key chain
-    if delta > timeout then
-        reset_state()
-    end
-
-    mod.last = now
+-- @return string
+-- @param key string
+-- @param recurse bool
+-- Try to resolve a key into a mapping, returning the string to be inserted. If
+-- the `recurse` is specified, tries to recursively match the key if no mapping
+-- could be found.
+local function resolve_key(key, recurse)
     mod.keys = mod.keys + 1
     local state = mod.state[key]
     local type = type(state)
@@ -51,7 +43,12 @@ local function _key_pressed(key)
     -- There are no bindings for this combination
     if type == "nil" then
         reset_state()
-        return key
+        -- Try to start a new mapping
+        if recurse then
+            return resolve_key(key)
+        else -- Don't recurse to avoid infinite recursion
+            return key
+        end
         -- There are more branches
     elseif type == "table" then
         mod.state = state
@@ -68,13 +65,29 @@ local function _key_pressed(key)
         end
 
         res = ok and res or ""
+        -- In case the function returns nil
+        res = res or ""
         return finish_mapping(res)
     end
 end
 
--- This function runs on every keypress registered in a mapping
+-- This function runs on every keypress registered in a mapping. This is where
+-- most of the plugin's logic is.
 function M.key_pressed(key)
-    local keys = _key_pressed(key)
+    mod.last = mod.last or vim.fn.reltime()
+    local now = vim.fn.reltime()
+    -- reltime is in seconds but we need miliseconds
+    local delta = vim.fn.reltimefloat(vim.fn.reltime(mod.last, now)) * 1000
+    local timeout = config.settings().timeout
+
+    -- Start a new key chain
+    if delta > timeout then
+        reset_state()
+    end
+
+    mod.last = now
+
+    local keys = resolve_key(key, true)
     keys = vim.api.nvim_replace_termcodes(keys, true, true, true)
     return keys
 end
